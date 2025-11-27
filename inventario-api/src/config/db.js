2a1d1a191367d5663d3b1c90887bfc583c16dee0
Hernán Leonardo Quiroga Beltrán <hleonardoquirogab@hotmail.com>
2025-11-27 11:35:52 -0500

    bd configurada con sequelize

---
+++ inventario-api/src/config/db.js
@@ -0,0 +1,17 @@
+// Configuración de la conexión a la base de datos usando Sequelize
+// Se obtienen las variables de entorno desde el archivo .env
+
+const { Sequelize } = require('sequelize');
+// Crear una nueva instancia de Sequelize con los parámetros de conexión
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+    }
+);
+// Exportar la instancia de Sequelize para su uso en otros módulos  
+module.exports = sequelize;
